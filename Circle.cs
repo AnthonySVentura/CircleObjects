33f44cbfc6ec155a23d964cbd90f74cb4101b78e
AnthonySVentura <107430112+AnthonySVentura@users.noreply.github.com>
2022-07-06 08:30:16 -0400

    fineshed

---
+++ Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleObjects
+{
+    //Class
+    internal class Circle
+    {
+        private double Radius;
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double CalculateCurcumference()
+        {
+            return 2 * Math.PI * Radius;
+        }
+    }
+
+    //Methods
+    
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using CircleObjects;
+
+Console.WriteLine("Welcome to the Circle Tester");
+
+double radius = 0;
+Console.WriteLine("Enter a radius");
+
+while (true)
+{
+    Console.WriteLine("Enter a radius");
+
+    while (double.TryParse(Console.ReadLine(), out radius))
+    {
+        if (radius < 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine(CalculateCurcumference(radius));
+        }
+    }
+
+}
